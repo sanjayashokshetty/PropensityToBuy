157209b872a972854759cc55c81f296f2ef1ae4a
Sanjay <sanjayshetty2015@gmail.com>
2018-07-22 22:34:43 +0530

    neural cleanup

--- NN.py
+++ NN.py
@@ -1,26 +1,9 @@
-
-# coding: utf-8
-
-# In[16]:
-
+'''
+neural network model
+'''
 
 import pandas as pd
-
-
-# In[17]:
-
-
 data=pd.read_csv('mf_fin.csv')
-
-
-# In[18]:
-
-
-data.shape
-
-
-# In[24]:
-
 
 from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 from sklearn.neural_network import MLPClassifier
@@ -50,10 +33,7 @@ def model(X_train,X_test,y_train,y_test):
     print(mlp.predict_proba(X_test),y_test)
     return mlp,X_test,y_test,p_values,scores,pred
 
-
-# In[25]:
-
-
+#upsampling the data
 df_majority = data[data.res==0]
 df_minority = data[data.res==1]
 print(df_majority.shape)
@@ -75,10 +55,7 @@ user_id_test=df_test['ACC_ID']
 y_train=df_train['res']
 y_test=df_test['res']
 
-
-# In[26]:
-
-
+#Feature selection using mutual information
 test = SelectKBest(score_func=mutual_info_classif,k=50)
 fit = test.fit(X_train.as_matrix(),y_train.as_matrix())
 p_values=test.pvalues_
@@ -86,45 +63,14 @@ scores=test.scores_
 X_train = fit.transform(X_train.as_matrix())
 X_test=fit.transform(X_test.as_matrix())
 
+#get to know the selected featues
+for i,j in zip(columns,fit.get_support()):
+    if j==True:
+        print(i)
 
-# In[27]:
-
-
+#train the model and get test
 mlp,X_test,y_test,p_values,scores,pred=model(X_train,X_test,y_train.as_matrix(),y_test.as_matrix())
 
-
-# In[11]:
-
-
+#save model
 from sklearn.externals import joblib
 lmodel=joblib.load('model4_30.pkl')
-
-
-# In[271]:
-
-
-print(confusion_matrix(y_test,lmodel.predict(X_test)))
-
-
-# In[272]:
-
-
-joblib.dump(lmodel,'model4_30.pkl')
-
-
-# In[28]:
-
-
-class_0=[]
-class_1=[]
-c=0
-for i in lmodel.predict_proba(X_test):
-    class_0.append(i[0])
-    class_1.append(i[1])
-
-
-# In[33]:
-
-
-pd.DataFrame({'user_id':user_id_test,'Prob_class0':class_0,'Prob_class1':class_1,'actual':y_test,'pred':lmodel.predict(X_test)}).to_csv("analysis.csv")
-
